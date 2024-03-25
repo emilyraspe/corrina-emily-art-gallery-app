c75c01ab7586d211202048c5799eba7e42b25596
Emily Raspe <emilyraspe@icloud.com>
2024-03-25 13:45:05 +0100

    edit tests

--- pages/components/Spotlight/Spotlight.test.js
+++ pages/components/Spotlight/Spotlight.test.js
@@ -1,14 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import Spotlight from "./Spotlight";
 
+const mockImage = "image-url.jpg";
+const mockArtist = "Artist";
+
 test("art piece image is displayed", () => {
-  render(<Spotlight />);
+  render(<Spotlight image={mockImage} />);
   const image = screen.getByRole("img");
   expect(image).toBeInTheDocument();
+  expect(image).toHaveAttribute("src", mockImage);
 });
 
 test("art piece artist is displayed", () => {
-  render(<Spotlight />);
-  const artist = screen.getByRole("heading");
+  render(<Spotlight artist={mockArtist} />);
+  const artist = screen.getByRole("heading", { name: mockArtist });
   expect(artist).toBeInTheDocument();
 });
