a8e4fac39c4ea2b1752f179f139535adf1c3ebfc
Corrina Cornish <corrina.cornish@gmail.com>
2024-03-25 14:22:33 +0100

    formatting

--- pages/components/Navigation/Navigation.test.js
+++ pages/components/Navigation/Navigation.test.js
@@ -1,7 +1,6 @@
 import {render, screen} from "@testing-library/react";
 import { useRouter } from "next/router";
 import Layout from "../Layout/Layout";
-import userEvent from "@testing-library/user-event";
 
 jest.mock("next/router", () => ({
     useRouter() {
@@ -21,25 +20,3 @@ test("Navigation link labeled Spotlight & Pieces is displayed", () => {
     expect(spotlightLink).toHaveAttribute("href");
     expect(piecesLink).toHaveAttribute("href");
 })
-
-// test("Clicking Spotlight shows the SpotlightPage", () => {
-//     const pushMock = jest.fn();
-//     jest.mock("next/router", () => ({
-//       useRouter() {
-//         return {
-//           push: pushMock,
-//           asPath: "/",
-//         };
-//       },
-//     }));
-    
-//     render (<Layout></Layout>);
-//     const user = userEvent.setup();
-//     const spotlightLink = screen.getByText("Spotlight");
-//     user.click(spotlightLink);
-//     expect(pushMock).toHaveBeenCalledWith('/spotlight');
-// })
-
-// test("Clicking Pieces shows the ArtPiecesPage", () => {
-    
-// })
