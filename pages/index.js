65ed5b48618b2e1636c6776462f065dbcccdafa6
Corrina Cornish <corrina.cornish@gmail.com>
2024-03-25 10:45:24 +0100

    fetchData

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
-export default function HomePage() {
-  return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
-  );
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+export default function ArtPieces() {
+  const {data} = useSWR(`https://example-apis.vercel.app/api/art`, fetcher);
+  if (!data || data.length === 0) {
+    return <div>Loading...</div>;
+  }
+  console.log(data[0]);
+
+  // return (
+
+  // )
 }
+
