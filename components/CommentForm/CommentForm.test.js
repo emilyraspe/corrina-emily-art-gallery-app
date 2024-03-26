868972564b1eb27f0701deacbcc2654f383e8889
Corrina Cornish <corrina.cornish@gmail.com>
2024-03-26 15:43:19 +0100

    testing

---
+++ components/CommentForm/CommentForm.test.js
@@ -0,0 +1,15 @@
+import {screen, render} from "@testing-library/react";
+import CommentForm from "./CommentForm.js";
+
+test("After submitting the form, the comment is appended to the list of comments", async () => {
+    const mockComment = jest.fn();
+    render (<CommentForm addComment={mockComment}
+    ></CommentForm>)
+    const user = user.setup();
+
+
+
+})
+
+
+// After submitting the form, the comment is appended to the list of comments 🖼️
